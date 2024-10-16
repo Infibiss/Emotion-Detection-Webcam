e451bf58e3fa8f2502d44f7f2a52a9545de80239
Infibiss <murtazalievmatvey@gmail.com>
2024-10-16 23:27:53 +0300

    del trash

--- src/model.py
+++ src/model.py
@@ -2,33 +2,6 @@ import numpy as np
 import cv2
 from tensorflow.keras import models
 
-import pandas as pd
-from PIL import Image
-import os
-from tqdm import tqdm
-from tensorflow.keras.utils import to_categorical
-
-# Список для хранения данных
-data = []
-
-for dirname, _, filenames in os.walk('../data/train'):
-    for filename in tqdm(filenames):
-        if filename.endswith('.png'):
-            img_path = os.path.join(dirname, filename)
-            # Открытие и изменение размера изображения
-            img = Image.open(img_path)
-            img = img.resize((224, 224))  # Изменение размера
-            img_array = np.array(img).flatten()  # Преобразование в 1D массив
-            img_array = img_array / 255.0  # Нормализация значений пикселей (0-1)
-            # Добавление целевой переменной (эмоции)
-            data.append(np.append(img_array, dirname))  # Добавление эмоции в конец массива
-
-# Создание DataFrame и сохранение в CSV
-df = pd.DataFrame(data)
-print(df)
-exit(0)
-df.to_csv('emotion_dataset.csv', index=False, header=False)  # Сохранение без индекса и заголовка
-
 # <---- PARAMS ---->
 emotions_id = {0: 'angry', 1: 'disgusted', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
 img_size = (224, 224)
